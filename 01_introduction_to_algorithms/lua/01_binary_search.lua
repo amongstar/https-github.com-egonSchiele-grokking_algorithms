e3016960eee2463b183bfa429df418528cd76765
zhangjiong <zhangjiongwx@outlook.com>
2017-08-09 17:24:00 +0800

    Update 01_binary_search.lua

--- 01_introduction_to_algorithms/lua/01_binary_search.lua
+++ 01_introduction_to_algorithms/lua/01_binary_search.lua
@@ -1,10 +1,10 @@
-function binary_search(table, value)
+function binary_search(array, value)
     -- Lua arrays start with 1
-    local low, high = 1, #table
+    local low, high = 1, #array
     
     while low <= high do
         local mid = math.floor((low + high) / 2)
-        local guess = table[mid]
+        local guess = array[mid]
         
         if guess == value then
             return mid
@@ -18,7 +18,7 @@ function binary_search(table, value)
     return nil
 end
 
-local my_table = {1, 3, 5, 7, 9}
+local my_array = {1, 3, 5, 7, 9}
 
-print(binary_search(my_table, 3)) -- => 2
-print(binary_search(my_table, -1)) -- => nil
+print(binary_search(my_array, 3)) -- => 2
+print(binary_search(my_array, -1)) -- => nil
